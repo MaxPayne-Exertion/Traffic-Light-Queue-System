a728c5c452da05e713c62c83cd7592133947cd32
sha_hj <110622961+MaxPayne-Exertion@users.noreply.github.com>
2025-12-22 23:00:56 +0545

    Log vehicle ID with spawn time in traffic_generator
    Added timestamp to vehicle ID logging in traffic generator.

--- traffic_generator.cpp
+++ traffic_generator.cpp
@@ -1,5 +1,5 @@
 #include <fstream>
-#include <thread>
+
 #include <chrono>
 #include <cstdlib>
 #include <ctime>
@@ -30,16 +30,16 @@ int main() {
            int random_index = rand() % 4;  // rand() % 4 gives 0, 1, 2, or 3
            const char* filename = files[random_index];
            std::string vehicle_id = idgen();
+           auto now = std::chrono::system_clock::now();
+           auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+           auto value = now_ms.time_since_epoch();
+           long spawn_time = value.count();
 
            // Open and write to the file
            std::ofstream file(filename, std::ios::app);
            if (file.is_open()) {
-               file << "V " << vehicle_id << "\n";
+               file <<vehicle_id<<" "<<spawn_time<< "\n";
            }
-
-           // Wait 400-1200 milliseconds (0.4 to 1.2 seconds)
-           int delay = 400 + (rand() % 800);  // rand() % 800 gives 0-799
-           std::this_thread::sleep_for(std::chrono::milliseconds(delay));
        }
 
        return 0;
